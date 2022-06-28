a6b76ce54abc416c76ccdeb4fa1510b8972e3a32
Jason Fricano <44284799+jfricano@users.noreply.github.com>
2022-06-27 23:00:48 -0400

    add age to view, elim age 0

--- src/models/User.ts
+++ src/models/User.ts
@@ -30,7 +30,7 @@ export class User extends Model<UserProps> {
   }
 
   setRandomAge(): void {
-    const age = Math.round(Math.random() * 100);
+    const age = Math.ceil(Math.random() * 100);
     this.set({ age });
   }
 }
--- src/views/UserShow.ts
+++ src/views/UserShow.ts
@@ -8,6 +8,7 @@ export class UserShow extends View<User, UserProps> {
       <div>
         <h1>User Detail</h1>
         <div>User Name: ${this.model.get('name')}</div>
+        <div>User Age: ${this.model.get('age')}</div>
       </div>
     `;
   }
